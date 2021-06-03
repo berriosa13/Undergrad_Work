f58b40671c602bb1ada82fe6fb2cbbdd6ca5c0a6
berriosa13 <berriosa13@gmail.com>
2021-06-03 11:11:42 -0400

    Update app.js

--- CSC_496 Web Dev/app.js
+++ CSC_496 Web Dev/app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const router = require( './routes/homeRoutes' );
+const router = require('./routes/homeRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 /*
